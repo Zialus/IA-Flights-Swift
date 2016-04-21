7116918b134b80721401eb00e5c657aad0424313
Raul Ferreira <raulmendesferreira@gmail.com>
2016-04-22 00:46:30 +0100

    Added functions to allow the calculation of permutations of a list.
    Added a function to "move to the next day".

--- Sources/Flights/Auxiliary.swift
+++ Sources/Flights/Auxiliary.swift
@@ -30,3 +30,56 @@ func printdebug(string: String) -> () {
     }
 
 }
+
+func printfulldebug(string: String) -> () {
+
+    if FULLDEBUG == true{
+        print(string)
+    }
+}
+
+func nextDay(day: String) -> (String){
+
+    let nextday: String
+
+    switch day {
+    case "mo":
+        nextday = "tu"
+    case "tu":
+        nextday = "we"
+    case "we":
+        nextday = "th"
+    case "th":
+        nextday = "fr"
+    case "fr":
+        nextday = "sa"
+    case "sa":
+        nextday = "su"
+    case "su":
+        nextday = "mo"
+    default:
+        nextday = "YOU SHOULDN'T DO THAT"
+    }
+
+    return nextday
+
+}
+
+func between<T>(x: T, _ ys: [T]) -> [[T]] {
+    guard let (head, tail) = ys.decompose() else { return [[x]] }
+    return [[x] + ys] + between(x, tail).map { [head] + $0 }
+}
+
+
+func permutations<T>(xs: [T]) -> [[T]] {
+    guard let (head, tail) = xs.decompose() else { return [[]] }
+    return permutations(tail).flatMap { between(head, $0) }
+}
+
+
+extension Array {
+    func decompose() -> (Generator.Element, [Generator.Element])? {
+        guard let x = first else { return nil }
+        return (x, Array(self[1..<count]))
+    }
+}

a1b1a5be2e2cec62c29b841f6d81ec98709dcf2c
Raul Ferreira <raulmendesferreira@gmail.com>
2016-12-03 23:23:43 +0000

    Updated from https://github.com/raywenderlich/swift-algorithm-club/blob/66da2bc7bd13cb8722476c74699e0c8a935c3b76/Stack/Stack.swift

--- Sources/Flights/Stack.swift
+++ Sources/Flights/Stack.swift
@@ -1,34 +1,29 @@
 /*
-  Last-in first-out stack (LIFO)
-
-  Push and pop are O(1) operations.
-*/
+ Last-in first-out stack (LIFO)
+ Push and pop are O(1) operations.
+ */
 public struct Stack<T> {
-  private var array = [T]()
+    fileprivate var array = [T]()
 
-  public var isEmpty: Bool {
-    return array.isEmpty
-  }
-
-  public var count: Int {
-    return array.count
-  }
-
-  public mutating func push(_ element: T) {
-    array.append(element)
-  }
-
-  public mutating func pop() -> T? {
-    if isEmpty {
-      return nil
-    } else {
-      return array.removeLast()
+    public var isEmpty: Bool {
+        return array.isEmpty
     }
-  }
 
-  public func peek() -> T? {
-    return array.last
-  }
+    public var count: Int {
+        return array.count
+    }
+
+    public mutating func push(_ element: T) {
+        array.append(element)
+    }
+
+    public mutating func pop() -> T? {
+        return array.popLast()
+    }
+
+    public func peek() -> T? {
+        return array.last
+    }
 }
 
 extension Stack: Sequence {
